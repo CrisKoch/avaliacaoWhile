a78c63ac95ba1194c692a3c23bd75e036cab9152
CrisKoch <cristinak.iftm@gmail.com>
2020-05-29 19:16:34 -0300

    Leitura dos dados

--- src/avaliacaoWhile.java
+++ src/avaliacaoWhile.java
@@ -7,6 +7,38 @@ public class avaliacaoWhile {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		String nome;
+		int horasTrabalhadas;
+		double valorPorHora;
+		char resposta;
+		
+		//Leitura dos dados
+		do {
+		System.out.print("Nome: ");
+		nome = sc.next();
+		
+		System.out.print("Horas trabalhadas: ");
+		horasTrabalhadas = sc.nextInt();
+			while (horasTrabalhadas < 0) {
+			horasTrabalhadas = sc.nextInt();
+			}
+			
+		System.out.print("Valor por hora: ");
+		valorPorHora = sc.nextDouble();
+			while (valorPorHora < 0) {
+			valorPorHora = sc.nextInt();
+			}
+		
+		System.out.print("Digitar outro (S/N) ");
+		resposta = sc.next().charAt(0);
+		}
+		while (resposta == 's' || resposta == 'S');
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
