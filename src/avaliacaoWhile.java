e142840f695f536a48b715bb0ed3cbb7228840e6
CrisKoch <cristinak.iftm@gmail.com>
2020-05-29 19:36:11 -0300

    Menu repetindo e correcao validacao

--- src/avaliacaoWhile.java
+++ src/avaliacaoWhile.java
@@ -8,7 +8,7 @@ public class avaliacaoWhile {
 		Scanner sc = new Scanner(System.in);
 
 		String nome;
-		int horasTrabalhadas;
+		int horasTrabalhadas, opcao;
 		double valorPorHora;
 		char resposta;
 		
@@ -20,22 +20,37 @@ public class avaliacaoWhile {
 		System.out.print("Horas trabalhadas: ");
 		horasTrabalhadas = sc.nextInt();
 			while (horasTrabalhadas < 0) {
+			System.out.print("Horas trabalhadas: ");
 			horasTrabalhadas = sc.nextInt();
 			}
 			
 		System.out.print("Valor por hora: ");
 		valorPorHora = sc.nextDouble();
 			while (valorPorHora < 0) {
+			System.out.print("Valor por hora: ");
 			valorPorHora = sc.nextInt();
 			}
 		
 		System.out.print("Digitar outro (S/N) ");
 		resposta = sc.next().charAt(0);
+		
+		
 		}
 		while (resposta == 's' || resposta == 'S');
 		
+		//Menu repetindo
 		
-		
+		do {
+		System.out.println();
+		System.out.println("MENU ");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair ");
+		System.out.println("Digite uma opção: ");
+		opcao = sc.nextInt();
+		}
+		while( opcao != 4);
 		
 		
 		sc.close();
